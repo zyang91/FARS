930877125c20f72880a50641d3f6a7ee415ae12e
zyang91 <zhanchao@upenn.edu>
2024-11-17 12:24:19 -0500

    feed more data in

--- zy/Tranditional_data/datawarggling.R
+++ zy/Tranditional_data/datawarggling.R
@@ -4,3 +4,14 @@ setwd("/Users/apple/FARS/zy/Tranditional_data")
 con <- dbConnect(duckdb::duckdb(), dbdir = "crash.duckdb")
 
 dbListTables(con)
+
+FARS2022<- dbGetQuery(con, "SELECT * FROM person2022")
+
+## No age data
+FARS2022_noage<- FARS2022 %>% 
+  filter(AGE==999|AGE==998)%>%
+  select(STATE,AGE,COUNTY)
+
+
+
+
