b9b6331140da7f63ae6d0123c07bf51ee7b68941
zyang91 <zhanchao@upenn.edu>
2024-11-21 10:19:27 -0500

    code for duckdb query

--- zy/Tranditional_data/datawarggling.R
+++ zy/Tranditional_data/datawarggling.R
@@ -1,18 +1,43 @@
 library(duckdb)
 library(tidyverse)
-setwd("/Users/apple/Desktop/FARS/zy/Tranditional_data")
+setwd("C:/Users/zyang/OneDrive/Desktop/FARS/zy/Tranditional_data")
 con <- dbConnect(duckdb::duckdb(), dbdir = "raw_data.duckdb")
 
 dbListTables(con)
 
 FARS2022<- dbGetQuery(con, "SELECT * FROM person2022")
 
-## No age data
-FARS2022_noage<- FARS2022 %>% 
-  filter(AGE==999|AGE==998)%>%
-  select(STATE,AGE,COUNTY)
+#close connection
+dbDisconnect(con, shutdown = TRUE)
+
+#selection of useful variable
+FARS2022_filter<- FARS2022 %>% 
+  select(ST_CASE,
+         STATE,
+         STATENAME,
+         COUNTY,
+         AGE,
+         AGENAME,
+         SEX,
+         INJ_SEV,
+         INJ_SEVNAME)
+
+## Notes: 1. connect duckdb database, 2. bring in the whole table into R varaibles
+## 3. select useful variables using tidyverse(dplyr) package 
+## 4. close the connection after finished
+## Please do not write any additional table into this dataset. It will be easier to keep only raw data into this one.
+
+
+
+
+
+
 
 #close connection
 dbDisconnect(con, shutdown = TRUE)
 
 
+## No age data
+FARS2022_noage<- FARS2022 %>% 
+  filter(AGE==999|AGE==998)%>%
+  select(STATE,AGE,COUNTY)
