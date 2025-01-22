e7eb0adb81bcb25e44571b126161f324c26d6550
zyang91 <zhanchao@upenn.edu>
2025-01-22 17:51:28 -0500

    updated calculation rate by cbsa

--- zy/data/calculate.R
+++ zy/data/calculate.R
@@ -1,4 +1,4 @@
-setwd("C:/Users/zyang/OneDrive/Desktop/data")
+setwd("C:/Users/zyang/OneDrive/Desktop/FARS/zy/data")
 library(tidyverse)
 library(tidycensus)
 
@@ -14,22 +14,14 @@ fata<-fata%>%
 
 full<- full_join(fata,pop, by=c("GEOID","year"))
 
-fata_al<- fata%>%
-  filter(STATE==1)%>%
-  group_by(COUNTY)%>%
-  summarise(total_fatalities=sum(fatality_count))
-
-write.csv(full, "full.csv")
 
 
-# read the data 
-full<-read_csv("full.csv")
 
 full<-full%>%
   select(GEOID, year, AGE_CATEGORY, fatality_count, AGE0_4, AGE5_9, AGE10_14, AGE15_17, AGE18_19)
 
 # calculate the percentage of fatalities in each age group
-calculated_rate<- full%>%
+full<- full%>%
   mutate(rate= ifelse(AGE_CATEGORY=="1", fatality_count/AGE0_4*1000,
                       ifelse(AGE_CATEGORY=="2", fatality_count/AGE5_9*1000,
                              ifelse(AGE_CATEGORY=="3", fatality_count/AGE10_14*1000,
@@ -45,6 +37,40 @@ calculated_rate<- full%>%
 rate<-calculated_rate%>%
   select(GEOID, year, AGE_CATEGORY, fatality_count, rate)
 
-# export the data
-write.csv(rate, "calaculate_rate.csv")
-write.csv(calculated_rate,"fullwithrate.csv")
+full<-full%>%
+  filter(!is.na(rate))%>%
+  filter(year>2016)%>%
+  filter(year!=2020)
+
+fars_2017<-full%>%
+  filter(year==2017)
+
+library(readxl)
+cbsa<-read_excel("cbsa_list.xlsx")
+
+cbsa<- cbsa%>%
+  mutate(GEOID=sprintf("%02d%03d", as.numeric(cbsa$`FIPS State Code`), as.numeric(cbsa$`FIPS County Code`)))
+
+cbsa<-cbsa%>%
+  select(`CBSA Code`, GEOID,`CBSA Title`,`Metropolitan/Micropolitan Statistical Area`,`CSA Title`,`County/County Equivalent`)
+
+full_joined<-full_join(fars_2017,cbsa, by="GEOID")
+
+full_join_filter<-full_joined%>%
+  filter(!is.na(year))
+
+cbsa_group<-full_join_filter%>%
+  group_by(`CBSA Code`,`CBSA Title`, AGE_CATEGORY)%>%
+  summarise(fatality_count=sum(fatality_count), 
+            AGE0_4=sum(AGE0_4),
+            AGE5_9=sum(AGE5_9),
+            AGE10_14=sum(AGE10_14),
+            AGE15_17=sum(AGE15_17),
+            AGE18_19=sum(AGE18_19))
+
+cbsa_group_rate<- cbsa_group%>%
+  mutate(rate= ifelse(AGE_CATEGORY=="1", fatality_count/AGE0_4*1000,
+                      ifelse(AGE_CATEGORY=="2", fatality_count/AGE5_9*1000,
+                             ifelse(AGE_CATEGORY=="3", fatality_count/AGE10_14*1000,
+                                    ifelse(AGE_CATEGORY=="4", fatality_count/AGE15_17*1000,
+                                           ifelse(AGE_CATEGORY=="5", fatality_count/AGE18_19*1000, NA))))))
