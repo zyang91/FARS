a7f4332988ddc6765d6b498be80c511a3aaf32c0
zyang91 <zhanchao@upenn.edu>
2025-01-30 11:08:33 -0500

    updated

--- zy/data/agg/updated/mapping.R
+++ zy/data/agg/updated/mapping.R
@@ -16,6 +16,12 @@ highest_pop<- fars5yrs %>%
   arrange(desc(pop_under18)) %>%
   slice_head(n=50)
 
+highest_pop30<- fars5yrs %>% 
+  arrange(desc(pop_under18)) %>%
+  slice_head(n=30)
+
+highest_pop30<- highest_pop30 %>% 
+  arrange(desc(rate))
 f2017<- read_csv("cbsa_agg_rate2017.csv")
 f2018<- read_csv("cbsa_agg_rate2018.csv")
 f2019<- read_csv("cbsa_agg_rate2019.csv")
@@ -55,3 +61,35 @@ highest10_rate<- fars5yrs %>%
 
 full2<- full %>%
   filter(cbsa_code %in% highest10_rate$cbsa_code)
+
+ggplot(full2, aes(x=year, y=rate, group=cbsa_name, color=cbsa_name))+
+  geom_line()+
+  geom_point()+
+  theme_minimal()+
+  labs(title="Top 10 CBSA with Highest Rate", x="Year", y="Rate")+
+  theme(legend.position = "right", legend.text = element_text(size=5), title = element_text(size=15))
+
+
+write.csv(highest_rate, "highest_rate50.csv")
+write.csv(highest_pop, "highest_pop50.csv")
+
+ggplot(highest_pop30,aes(x=rate,y=reorder(cbsa_name,rate),fill=pop_under18))+
+  geom_col()+
+  scale_fill_gradient(low="#56B1F7", high="#132B43", name="Population")+
+  labs(
+    title="CBSA with Highest Population Under 18",
+    subtitle = "Top 30 most populated, 2017 - 2022",
+    x="Fatality Rate (per 100,000)",
+    y="Metropolitan Area"
+  )+
+  theme_minimal(base_size=12)+
+  theme(
+    plot.title = element_text(hjust = 0.5, face = "bold"),
+    plot.subtitle = element_text(hjust = 0.5),
+    axis.text.y = element_text(size = 8),
+    axis.title.x = element_text(size = 12),
+    axis.title.y = element_text(size = 12),
+    legend.title = element_text(size = 10),  # Smaller legend title
+    legend.text = element_text(size = 8),   # Smaller legend text
+    legend.key.size = unit(0.6, "lines") 
+  )
